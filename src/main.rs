a06ad0ffaaf268047e6b3ed27df8e86410f6e08f
Esteban Victorio <estebanvictorio92@gmail.com>
2020-11-15 20:29:09 -0800

    feature: add book pages to follow when checking web app again

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_web_app::MessageApp;
-
+// NOTE: last page: 70
+// NOTE: next page: 71
 fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG","actix_web=info");
     env_logger::init();
