413670fc7d6943f5abeec142ae97de202d062d86
Esteban Victorio <estebanvictorio92@gmail.com>
2021-01-04 19:23:21 -0800

    feature: add bookmark to last page

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,19 @@
 #[macro_use]
 extern crate actix_web;
 
-use actix_web::{ middleware, web, App, HttpRequest, HttpServer, Result };
-use serde::{Serialize, Deserialize};
+use actix_web::{middleware, web, App, HttpRequest, HttpServer, Result};
+use serde::{Deserialize, Serialize};
 use std::cell::Cell;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::{Arc, Mutex};
 
 static SERVER_COUNTER: AtomicUsize = AtomicUsize::new(0);
 
-
-
 struct AppState {
   server_id: usize,
   request_count: Cell<usize>,
   messages: Arc<Mutex<Vec<String>>>,
 }
-
 
 #[derive(Deserialize)]
 struct PostInput {
@@ -41,8 +38,7 @@ struct IndexResponse {
   messages: Vec<String>,
 }
 
-
-fn post(msg:web::Json<PostInput>, state: web::Data<AppState>) -> Result<web::Json<PostResponse>> {
+fn post(msg: web::Json<PostInput>, state: web::Data<AppState>) -> Result<web::Json<PostResponse>> {
   let request_count = state.request_count.get() + 1;
   state.request_count.set(request_count);
   let mut ms = state.messages.lock().unwrap();
@@ -62,20 +58,19 @@ fn clear(state: web::Data<AppState>) -> Result<web::Json<IndexResponse>> {
   let mut ms = state.messages.lock().unwrap();
   ms.clear();
 
-  Ok(web::Json(IndexResponse{
+  Ok(web::Json(IndexResponse {
     request_count,
     server_id: state.server_id,
     messages: vec![],
   }))
 }
 
-// NOTE: last page - 81 at "Receiving input"
+// NOTE: last page - 87 at "Receiving input"
 #[get("/")]
 fn index(state: web::Data<AppState>) -> Result<web::Json<IndexResponse>> {
   let request_count = state.request_count.get() + 1;
   state.request_count.set(request_count);
   let ms = state.messages.lock().unwrap(); // RAII (Resource Acquisition Is Initialization)
-  
   Ok(web::Json(IndexResponse {
     server_id: state.server_id,
     request_count,
@@ -85,7 +80,7 @@ fn index(state: web::Data<AppState>) -> Result<web::Json<IndexResponse>> {
 
 impl MessageApp {
   pub fn new(port: u16) -> Self {
-    MessageApp { port, }
+    MessageApp { port }
   }
 
   pub fn run(&self) -> std::io::Result<()> {
@@ -93,17 +88,17 @@ impl MessageApp {
     let messages = Arc::new(Mutex::new(vec![]));
     HttpServer::new(move || {
       App::new()
-      .data(AppState{
-        server_id: SERVER_COUNTER.fetch_add(1,Ordering::SeqCst),
-        request_count: Cell::new(0),
-        messages: messages.clone(),
-      })
+        .data(AppState {
+          server_id: SERVER_COUNTER.fetch_add(1, Ordering::SeqCst),
+          request_count: Cell::new(0),
+          messages: messages.clone(),
+        })
         .wrap(middleware::Logger::default())
         .service(index)
         .service(
           web::resource("/send")
-                      .data(web::JsonConfig::default().limit(4096))
-                      .route(web::post().to(post))
+            .data(web::JsonConfig::default().limit(4096))
+            .route(web::post().to(post)),
         )
         .service(clear)
     })
